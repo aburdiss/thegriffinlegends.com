909ab0dd1fca1dd2352546eefbe33547808d8d55
Alexander Burdiss <aburdiss@icloud.com>
2025-02-15 12:58:19 -0500

    feat: Add dynamic content to books page header

---
+++ books/books.js
@@ -0,0 +1,31 @@
+let PROJECT_ID = 'qqm8v4gw';
+let DATASET = 'production';
+const PAGE_NAME = 'books';
+let QUERY = encodeURIComponent('*[_type == "' + PAGE_NAME + '"]');
+let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+const firstSectionHeadlineContainer = document.querySelector(
+  '#first-section-headline'
+);
+const firstSectionTextContainer = document.querySelector('#first-section-text');
+
+const firstSectionLinkContainer = document.querySelector(
+  '#first-section-link-container'
+);
+
+// fetch the content
+fetch(URL)
+  .then((res) => res.json())
+  .then(({ result }) => {
+    const page = result.find((obj) => obj?._id === PAGE_NAME);
+
+    // Put data in books page
+    const { firstSectionHeadline, firstSectionText, firstSectionLink } = page;
+
+    firstSectionHeadlineContainer.innerText = firstSectionHeadline;
+    sanityBlockContent(firstSectionTextContainer, firstSectionText);
+    if (!!firstSectionLink) {
+      firstSectionLinkContainer.innerHTML = `<a class="button" href="${firstSectionLink.url}">${firstSectionLink.title}</a>`;
+    }
+  })
+  .catch((err) => console.error(err));
