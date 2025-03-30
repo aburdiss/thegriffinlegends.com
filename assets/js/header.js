9f55c3d6bc7eb547af829335a68bc47a15d48cd8
Alexander Burdiss <aburdiss@icloud.com>
2025-03-30 14:26:30 -0400

    fix: fix some styles
    - fix footer on books page
    - fix textarea on contact page
    - fix header on news page

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,8 @@
 document.body.classList.add('js-enabled');
 const navContainer = document.querySelector('header nav');
 
-const subdomain = '';
-// const subdomain = 'thegriffinlegends.com/';
+// const subdomain = '';
+const subdomain = 'thegriffinlegends.com/';
 
 navContainer.innerHTML = `
 <ul>
