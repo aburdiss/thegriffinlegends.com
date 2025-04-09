7dd5acb159a0c439fc5920b50ebd85ba6c357f68
Alexander Burdiss <aburdiss@icloud.com>
2025-04-08 20:00:32 -0400

    feat: add news and events pages

--- news/news.js
+++ news/news.js
@@ -1,68 +1,99 @@
-const newsContainer = document.querySelector('#news-container');
-newsContainer.innerHTML = 'Loading News...';
-
-const upcomingNewsContainer = document.createElement('div');
-upcomingNewsContainer.classList.add('upcoming-news-container');
-
-const pastNewsContainer = document.createElement('div');
-pastNewsContainer.classList.add('past-news-container');
-
 let PROJECT_ID = 'qqm8v4gw';
 let DATASET = 'production';
-let QUERY = encodeURIComponent('*[_type == "news"]');
+const PAGE_NAME = 'newsEventPage';
+let QUERY = encodeURIComponent(`*[_type in ["${PAGE_NAME}", "news", "event"]]{
+  _type == "${PAGE_NAME}" => {
+    _type,
+    mainHeadline,
+    eventsHeadline,
+    newsHeadline
+  },
+  _type == "news" => {
+    _type,
+    headline,
+    shortDescription,
+    description,
+    date,
+    slug,
+    "imageUrl": image.asset->url,
+    "imageAlt": image.alt
+  },
+  _type == "event" => {
+    _type,
+    headline,
+    description,
+    date,
+    address1,
+    address2,
+    address3,
+    "imageUrl": image.asset->url,
+    "imageAlt": image.alt
+  }
+}`);
 let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+const pageHeadlineContainer = document.querySelector(
+  '#page-headline-container'
+);
+
+const newsContainer = document.querySelector('#news-container');
+const newsHeadlineContainer = document.querySelector(
+  '#news-headline-container'
+);
+
+const eventsContainer = document.querySelector('#events-container');
+const eventsHeadlineContainer = document.querySelector(
+  '#events-headline-container'
+);
+
+newsContainer.innerHTML = `<p>Loading News...</p>`;
+eventsContainer.innerHTML = `<p>Loading Events...</p>`;
 
 // fetch the content
 fetch(URL)
   .then((res) => res.json())
   .then(({ result }) => {
-    const upcomingNews = [];
-    const pastNews = [];
+    console.log(result);
+    const events = result
+      .filter(function (content) {
+        return content._type === 'event';
+      })
+      .filter(function (event) {
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        return new Date(event.date).getTime() > tomorrow.getTime();
+      });
+    const news = result.filter(function (content) {
+      return content._type === 'news';
+    });
+    const pageData = result.find(function (content) {
+      return content._type === 'newsEventPage';
+    });
 
-    if (result.length === 0) {
+    pageHeadlineContainer.innerText = pageData.mainHeadline;
+    newsHeadlineContainer.innerText = pageData.newsHeadline;
+    eventsHeadlineContainer.innerText = pageData.eventsHeadline;
+
+    if (news.length === 0) {
       newsContainer.innerHTML = `
       <p>There is no news to display at this time.</p>
       <p>Please check back later!</p>`;
-      return;
+    } else {
+      newsContainer.innerHTML = createNews(news);
     }
 
-    result.forEach(function (news) {
-      const newsDate = new Date(news.date);
-      const today = new Date();
-      if (newsDate >= today) upcomingNews.push(news);
-      else pastNews.push(news);
-    });
-    console.log({ upcomingNews, pastNews });
-
-    newsContainer.innerHTML = '';
-
-    upcomingNewsContainer.innerHTML = `
-    <h2>News and Events</h2>
-    <div class="upcoming news-container">
-      ${
-        upcomingNews.length === 0
-          ? `<p>There are no upcoming events.</p>
-             <p>Please check back later!</p>`
-          : createEvents(upcomingNews)
-      }
-    </div>`;
-
-    pastNewsContainer.innerHTML = `
-    <h2>Past News and Events</h2>
-    <div class="past news-container">
-      ${
-        pastNews.length === 0
-          ? `<p>There are no past events.</p>
-             <p>Please check back later!</p>`
-          : createEvents(pastNews)
-      }
-    </div>`;
-
-    newsContainer.appendChild(upcomingNewsContainer);
-    newsContainer.appendChild(pastNewsContainer);
+    if (events.length === 0) {
+      eventsContainer.innerHTML = `
+      <p>There are no Events to display at this time.</p>
+      <p>Please check back later!</p>`;
+    } else {
+      eventsContainer.innerHTML = createEvents(events);
+    }
   })
   .catch((err) => {
-    newsContainer.innerHTML = `<p>Failed to load news.</p>
+    document.querySelector('.overlay.events').remove();
+    newsHeadlineContainer.innerHTML = 'Failed to load news and events.';
+    newsContainer.innerHTML = `<br />
 <p>If this persists, please <a href="mailto:thegriffinlegends@alexanderburdiss.com?subject=${encodeURIComponent(
       'The Griffin Legends Error'
     )}&body=${encodeURIComponent(
@@ -73,9 +104,71 @@ fetch(URL)
   });
 
 function createEvents(eventList) {
-  const eventsMarkdownList = eventList.map(function (event) {
-    return `<h3>${event.headline}</h3>`;
-  });
+  const eventsMarkdownList = eventList
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date);
+    })
+    .map(function (event) {
+      return `
+<div class="event-card">
+  ${
+    !!event.imageUrl
+      ? `<div class="left">
+          <img src="${event.imageUrl}" alt="${event.imageAlt}" />
+        </div>`
+      : ''
+  }
+  <div class="right">
+   <h3>${event.headline}</h3>
+    <time datetime="${event.date}">
+      ${new Date(event.date).toLocaleDateString()}
+    </time>
+    <div class="location">
+      <div>${event.address1}</div>
+      <div>${event.address2}</div>
+      <div>${event.address3}</div>
+    </div>
+    <hr />
+    <div class="description">
+      ${
+        sanityBlockContent(document.createElement('div'), event.description)
+          .outerHTML
+      }
+    </div>
+  </div>
+</div>`;
+    });
 
   return eventsMarkdownList.join('');
+}
+
+function createNews(newsList) {
+  const newsMarkdownList = newsList
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date);
+    })
+    .map(function (news) {
+      return `
+<a class="news-card" href="./article#${news.slug.current}">
+  ${
+    !!news.imageUrl
+      ? `<img src="${news.imageUrl}" alt="${news.imageAlt}" />`
+      : ''
+  }
+  <div class="left">
+    <h3>${news.headline}</h3>
+    <time datetime="${news.date}">
+      ${new Date(news.date).toLocaleDateString()}
+    </time>
+    <div class="description">
+      ${
+        sanityBlockContent(document.createElement('div'), news.shortDescription)
+          .outerHTML
+      }
+    </div>
+  </div>
+</a>`;
+    });
+
+  return newsMarkdownList.join('');
 }
